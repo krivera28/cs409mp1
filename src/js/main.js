eeaac153d0ecbe649bffe056ac7fcaa9223e1d54
Kaylani R <krive5@illinois.edu>
2026-09-22 15:16:25 -0500

    small navbar fix

--- src/js/main.js
+++ src/js/main.js
@@ -17,7 +17,7 @@ function scrollFunction(){
     for(const section of sections){
         const sectionTop = section.getBoundingClientRect().top;
 
-        if(sectionTop <= navbarBottom){
+        if(sectionTop <= (navbarBottom + 10)){
             currentSection = section;
         }
     }
